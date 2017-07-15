9bfbeff237aa50c13277a541996007424ec22f01
jhesed.tacadena <jhesed.tacadena@gmail.com>
2017-07-15 01:22:02 -0400

    Added base android files

---
+++ app/src/main/java/com/jjhsoftware/prayerjournal/TodayPrayer.java
@@ -0,0 +1,13 @@
+package com.jjhsoftware.prayerjournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TodayPrayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today_prayer);
+    }
+}
